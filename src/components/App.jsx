7f74eb9ae90db83bb2e144d18071e25d01df4004
Bohdan Izbash <olgaizbash@gmail.com>
2023-09-23 23:01:11 +0300

    sec

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
-import { nanoid } from 'nanoid';
 import './App.module.css';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
